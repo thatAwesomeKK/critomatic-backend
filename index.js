dca14930b585ac52bb480c6042f50ef55809bf25
thatAwesomeKK <2550492.kk@gmail.com>
2022-04-12 01:39:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ const port = process.env.PORT || 5000
 const cors = require('cors')
 require('dotenv').config()
 
-app.get('/', (req, res)=>{
-    res.send("Hello World")
-})
 app.use(cors())
 app.use(express.json({ limit: '1000mb' }))
 
 app.use('/api/getcontent',require('./routes/fetchContent'))
 app.use('/api/addpeople',require('./routes/addPeople'))
+
+app.get('/', (req, res)=>{
+    res.send("Hello World")
+})
 
 app.listen(port, ()=>{
     console.log(`Application started on Port : ${port}`);
