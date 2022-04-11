e915ad8eb5684a1b779abfce2dea2b2ba16de95d
thatAwesomeKK <2550492.kk@gmail.com>
2022-04-12 01:47:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express')
 const app = express()
 const connectToMongo = require('./db')
 connectToMongo()
+require('dotenv').config()
 const port = process.env.PORT || 5000
 const cors = require('cors')
-require('dotenv').config()
+
 
 app.use(cors())
 app.use(express.json({ limit: '1000mb' }))
