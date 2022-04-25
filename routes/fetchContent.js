7a96e2db9759b8c390bcce50bb3229b70eee937a
thatAwesomeKK <2550492.kk@gmail.com>
2022-04-25 19:41:36 +0530

    Update fetchContent.js

--- routes/fetchContent.js
+++ routes/fetchContent.js
@@ -38,7 +38,7 @@ router.get("/get-movie", async (req, res) => {
 //Get all the slugs to use getStaticProps
 router.get("/get-slugs", async (req, res) => {
   try {
-    let slugs = await Movie.find({}).select('slug')
+    let slugs = await MovieInfo.find({}).select('slug')
     return res.status(200).json(slugs);
   } catch (error) {
     return res.status(500).json(error);
