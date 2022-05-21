d08ef8abc89ea5d9f95fa8029e5dd63ea167b7a2
thatAwesomeKK <2550492.kk@gmail.com>
2022-05-21 16:37:51 +0530

    Changed Expiry times for tokens

--- methods/jwtCreation.js
+++ methods/jwtCreation.js
@@ -4,11 +4,11 @@ const jwtAccessSecret = process.env.JWT_ACCESS_SECRET;
 const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
 
 const getAccessToken = async(data)=>{
-    return jwt.sign(data, jwtAccessSecret, { expiresIn: '5m' }); 
+    return jwt.sign(data, jwtAccessSecret, { expiresIn: '12h' }); 
 }
 
 const getRefreshToken = async(data)=>{
-    return jwt.sign(data, jwtRefreshSecret, { expiresIn: '15m' });
+    return jwt.sign(data, jwtRefreshSecret, { expiresIn: '1d' });
 }   
 
 module.exports = {getAccessToken, getRefreshToken}
